e264c08ced1cc85244dac814bf8b330ac899a497
upendra7950 <83034381+upendra7950@users.noreply.github.com>
2021-08-20 00:08:45 +0530

    initial commit

---
+++ app/src/main/java/com/example/mad_tute4/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.mad_tute4.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
